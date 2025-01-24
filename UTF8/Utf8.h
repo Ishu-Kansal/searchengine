1ec3ccfea26d9621b81a9e6dd3352369e5c70fef
Ishu-Kansal <ikansal22@gmail.com>
2025-01-23 21:45:02 -0500

    partially working utf-8

--- UTF8/Utf8.h
+++ UTF8/Utf8.h
@@ -6,15 +6,13 @@
 // writing UTF-8 characters, translating to Unicode, and comparing UTF-8
 // strings.
 
-
 #pragma once
 
 #include <cstddef>
 #include <cstdint>
 
 typedef uint16_t Unicode;
-typedef uint8_t  Utf8;
-
+typedef uint8_t Utf8;
 
 // From http://www.cl.cam.ac.uk/~mgk25/unicode.html#utf-8
 
@@ -24,8 +22,9 @@ typedef uint8_t  Utf8;
 //   U-00000080 - U-000007FF:   110xxxxx 10xxxxxx 11 bits
 //   U-00000800 - U-0000FFFF:   1110xxxx 10xxxxxx 10xxxxxx 16 bits
 //   U-00010000 - U-001FFFFF:   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx 21 bits
-//   U-00200000 - U-03FFFFFF:   111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 26 bits
-//   U-04000000 - U-7FFFFFFF:   1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 31 bits
+//   U-00200000 - U-03FFFFFF:   111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 26
+//   bits U-04000000 - U-7FFFFFFF:   1111110x 10xxxxxx 10xxxxxx 10xxxxxx
+//   10xxxxxx 10xxxxxx 31 bits
 //
 //   Invalid, Overlong UTF-8 Sequences which should never be accepted
 //   because of the security risk:
@@ -39,7 +38,7 @@ typedef uint8_t  Utf8;
 // we will follow convention, which is to replace any malformed characters
 // with the replacement character, 0xfffd.
 
-const Unicode  ReplacementCharacter = 0xfffd;
+const Unicode ReplacementCharacter = 0xfffd;
 
 // Utf8 and Unicode text files are often marked with a byte order
 // mark.  If you try to read a file as either Utf8 or Unicode and
@@ -48,12 +47,12 @@ const Unicode  ReplacementCharacter = 0xfffd;
 // If there's no BOM on a text file, it's probably ASCII but might
 // be ANSI with the high half (0x80 to 0xff) internationalized.
 
-const Unicode  ByteOrderMark = 0xfeff;
+const Unicode ByteOrderMark = 0xfeff;
 
 // The Utf8 byte order mark is the same 0xfeff BOM character value
 // but written out as Utf8.
 
-const Utf8     Utf8BOMString[ ] = { 0xef, 0xbb, 0xbf };
+const Utf8 Utf8BOMString[] = {0xef, 0xbb, 0xbf};
 
 // SizeOfUTF8 tells the number of bytes it will take to encode the
 // specified value.
@@ -62,77 +61,73 @@ const Utf8     Utf8BOMString[ ] = { 0xef, 0xbb, 0xbf };
 // the character pointed to by p because p may point to a malformed
 // character.
 
-size_t SizeOfUtf8( Unicode c );
+size_t SizeOfUtf8(Unicode c);
 
 // IndicatedLength looks at the first byte of a Utf8 sequence
 // and determines the expected length.  Useful for avoiding
 // buffer overruns when reading Utf8 characters.  Return 1
 // for an invalid first byte.
 
-size_t IndicatedLength( const Utf8 *p );
+size_t IndicatedLength(const Utf8 *p);
 
 // Get the UTF-8 character as a Unicode value.
 // If it's an invalid UTF-8 encoding for a U-16
 // character, return the special malformed
 // character code.
 
-Unicode GetUtf8( const Utf8 *p );
+Unicode GetUtf8(const Utf8 *p);
 
 // Bounded version.  bound = one past last valid
 // byte.  bound == 0 means no bounds checking.
 // If a character runs past the last valid byte,
 // return the replacement character.
 
-Unicode GetUtf8( const Utf8 *p, const Utf8 *bound );
+Unicode GetUtf8(const Utf8 *p, const Utf8 *bound);
 
 // NextUtf8 will determine the length of the Utf8 character at p
 // by examining the first byte, then scan forward that amount,
 // stopping early if it encounters an invalid continuation byte
 // or the bound, if specified.
 
-const Utf8 *NextUtf8( const Utf8 *p, const Utf8 *bound = nullptr );
-
+const Utf8 *NextUtf8(const Utf8 *p, const Utf8 *bound = nullptr);
 
 // Scan backward for the first PREVIOUS byte which could
 // be the start of a UTF-8 character.
 
-const Utf8 *PreviousUtf8( const Utf8 *p );
+const Utf8 *PreviousUtf8(const Utf8 *p);
 
 // Write a Unicode character in UTF-8, returning one past
 // the end.
 
-Utf8 *WriteUtf8( Utf8 *p, Unicode c );
-
-
+Utf8 *WriteUtf8(Utf8 *p, Unicode c);
 
 //
 // Following is not part of the homework.
 //
 
-
 // UTF-8 String compares.
 // Same return values as strcmp( ).
 
-int StringCompare( const Utf8 *a, const Utf8 *b );
+int StringCompare(const Utf8 *a, const Utf8 *b);
 
 // Unicode string compare up to 'N' UTF-8 characters (not bytes)
 // from two UTF-8 strings.
 
-int StringCompare( const Utf8 *a, const Utf8 *b, size_t N );
+int StringCompare(const Utf8 *a, const Utf8 *b, size_t N);
 
 // Case-independent compares.
 
-int StringCompareI( const Utf8 *a, const Utf8 *b );
-int StringCompareI( const Utf8 *a, const Utf8 *b, size_t N );
+int StringCompareI(const Utf8 *a, const Utf8 *b);
+int StringCompareI(const Utf8 *a, const Utf8 *b, size_t N);
 
 // Lower-case only Ascii characters < 0x80 except
 // spaces and control characters.
 
-Unicode ToLower( Unicode c );
+Unicode ToLower(Unicode c);
 
 // Identify Unicode code points representing Ascii
 // punctuation, spaces and control characters.
 
-bool IsPunctuation( Unicode c );
-bool IsSpace( Unicode c );
-bool IsControl( Unicode c );
+bool IsPunctuation(Unicode c);
+bool IsSpace(Unicode c);
+bool IsControl(Unicode c);
--- UTF8/flip.cpp
+++ UTF8/flip.cpp
@@ -9,42 +9,71 @@
 
 #include "Utf8.h"
 
-enum Mode
-{
-  ASCII,
-  UTF8,
-  UNICODE,
+struct return_t {
+    Utf8 *memStart;
+    long numBytes;
 };
 
-int getFileSize(int f)
-{
-  struct stat fileInfo;
-  fstat(f, &fileInfo);
-  return fileInfo.st_size;
+int GetFileSize(int f) {
+    struct stat fileInfo;
+    fstat(f, &fileInfo);
+    return fileInfo.st_size;
 }
 
-int main(int argc, char **argv)
-{
-  const int f = open(argv[1], O_RDONLY);
-  if (f == -1)
-  {
-    std::cerr << "Error opening file: " << argv[1] << " with error " << errno
-              << std::endl;
-    return;
-  }
-  size_t size = getFileSize(f);
-  char *file = (char *)(mmap(nullptr, size, PROT_READ, MAP_PRIVATE, f, 0));
-  char *end = file + size;
-  if (GetUtf8(reinterpret_cast<Utf8 *>(file)) == ByteOrderMark)
-  {
-  }
-  std::vector<Utf8> chars{};
-  chars.reserve(size);
-  for (size_t i = 0; i < size; ++i)
-  {
-    const char byte = file[i];
-    const Utf8 symbol = static_cast<Utf8>(byte);
-  }
+std::vector<Unicode> GetUnicodeMem(const Utf8 *fileStart, const Utf8 *fileEnd) {
+    std::vector<Unicode> chars{};
+    chars.reserve(fileEnd - fileStart);
+    chars.push_back(ByteOrderMark);
+    while (fileStart < fileEnd) {
+        chars.push_back(GetUtf8(fileStart, fileEnd));
+        fileStart = NextUtf8(fileStart, fileEnd);
+    }
+    return chars;
+}
 
-  write(STDOUT_FILENO, chars.data(), chars.size());
+return_t GetUtf8Mem(const Unicode *fileStart, const Unicode *fileEnd) {
+    size_t worstCase = (fileEnd - fileStart + 1) * 3;
+    Utf8 *mem = new Utf8[worstCase]();
+    Utf8 *start = mem;
+    start = WriteUtf8(start, ByteOrderMark);
+    while (fileStart < fileEnd) {
+        start = WriteUtf8(start, *fileStart);
+        ++fileStart;
+    }
+    return return_t{mem, start - mem};
+}
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        std::cerr << "Usage: flip <filename>\n";
+        std::cerr << "Convert Unicode to Utf8 and Utf8 or ASCII to Unicode and "
+                     "write to stdout.\n";
+        return 0;
+    }
+    const int f = open(argv[1], O_RDONLY);
+    if (f == -1) {
+        std::cerr << "Error opening file: " << argv[1] << " with error "
+                  << errno << std::endl;
+        close(f);
+        exit(EXIT_FAILURE);
+    }
+    size_t size = GetFileSize(f);
+    Utf8 *UtfFile = (Utf8 *)(mmap(nullptr, size, PROT_READ, MAP_PRIVATE, f, 0));
+    Utf8 *UtfEnd = UtfFile + size;
+    Unicode *UnicodeFile = reinterpret_cast<Unicode *>(UtfFile);
+    Unicode *UnicodeEnd = reinterpret_cast<Unicode *>(UtfEnd);
+    if (GetUtf8(UtfFile, UtfEnd) == ByteOrderMark) {
+        const std::vector<Unicode> unicode = GetUnicodeMem(UtfFile + 3, UtfEnd);
+        write(STDOUT_FILENO, unicode.data(),
+              unicode.size() * sizeof(unicode[0]));
+    } else if (UnicodeFile[0] == ByteOrderMark) {
+        const return_t data = GetUtf8Mem(UnicodeFile + 1, UnicodeEnd);
+        write(STDOUT_FILENO, data.memStart, data.numBytes);
+        delete[] data.memStart;
+    } else {
+        const std::vector<Unicode> unicode = GetUnicodeMem(UtfFile, UtfEnd);
+        write(STDOUT_FILENO, unicode.data(),
+              unicode.size() * sizeof(unicode[0]));
+    }
+    close(f);
 }
