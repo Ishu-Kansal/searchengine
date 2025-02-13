9919c502d62dfe1e68f9ea4eddf2d8e9727ddcfb
Ishu-Kansal <ikansal22@gmail.com>
2025-02-13 05:27:25 -0500

    started geturl

--- LinuxGetUrl/LinuxGetUrl.cpp
+++ LinuxGetUrl/LinuxGetUrl.cpp
@@ -1,107 +1,129 @@
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include <iostream>
 
-class ParsedUrl
-   {
+class ParsedUrl {
    public:
-      const char *CompleteUrl;
-      char *Service, *Host, *Port, *Path;
+    const char *CompleteUrl;
+    char *Service, *Host, *Port, *Path;
 
-      ParsedUrl( const char *url )
-         {
-         // Assumes url points to static text but
-         // does not check.
+    void printURL() {
+        std::cout << "url: " << CompleteUrl << std::endl;
+        std::cout << "service: " << Service << std::endl;
+        std::cout << "host: " << Host << std::endl;
+        std::cout << "port: " << Port << std::endl;
+        std::cout << "path: " << Path << std::endl;
+    }
 
-         CompleteUrl = url;
+    ParsedUrl(const char *url) {
+        // Assumes url points to static text but
+        // does not check.
 
-         pathBuffer = new char[ strlen( url ) + 1 ];
-         const char *f;
-         char *t;
-         for ( t = pathBuffer, f = url; *t++ = *f++; )
-            ;
+        CompleteUrl = url;
 
-         Service = pathBuffer;
+        pathBuffer = new char[strlen(url) + 1];
+        const char *f;
+        char *t;
+        for (t = pathBuffer, f = url; *t++ = *f++;);
 
-         const char Colon = ':', Slash = '/';
-         char *p;
-         for ( p = pathBuffer; *p && *p != Colon; p++ )
-            ;
+        Service = pathBuffer;
 
-         if ( *p )
-            {
+        const char Colon = ':', Slash = '/';
+        char *p;
+        for (p = pathBuffer; *p && *p != Colon; p++);
+
+        if (*p) {
             // Mark the end of the Service.
             *p++ = 0;
 
-            if ( *p == Slash )
-               p++;
-            if ( *p == Slash )
-               p++;
+            if (*p == Slash) p++;
+            if (*p == Slash) p++;
 
             Host = p;
 
-            for ( ; *p && *p != Slash && *p != Colon; p++ )
-               ;
+            for (; *p && *p != Slash && *p != Colon; p++);
 
-            if ( *p == Colon )
-               {
-               // Port specified.  Skip over the colon and
-               // the port number.
-               *p++ = 0;
-               Port = +p;
-               for ( ; *p && *p != Slash; p++ )
-                  ;
-               }
-            else
-               Port = p;
+            if (*p == Colon) {
+                // Port specified.  Skip over the colon and
+                // the port number.
+                *p++ = 0;
+                Port = +p;
+                for (; *p && *p != Slash; p++);
+            } else
+                Port = p;
 
-            if ( *p )
-               // Mark the end of the Host and Port.
-               *p++ = 0;
+            if (*p)
+                // Mark the end of the Host and Port.
+                *p++ = 0;
 
             // Whatever remains is the Path.
             Path = p;
-            }
-         else
+        } else
             Host = Path = p;
-         }
+    }
 
-      ~ParsedUrl( )
-         {
-         delete[ ] pathBuffer;
-         }
+    ~ParsedUrl() { delete[] pathBuffer; }
 
    private:
-      char *pathBuffer;
-   };
+    char *pathBuffer;
+};
 
-int main( int argc, char **argv )
-   {
+void printAddrInfo(addrinfo &info) {
+    std::cout << "ai addr: " << info.ai_addr << std::endl;
+    std::cout << "ai addrlen: " << info.ai_addrlen << std::endl;
+}
 
-   if ( argc != 2 )
-      {
-      std::cerr << "Usage: " << argv[ 0 ] << " url" << std::endl;
-      return 1;
-      }
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " url" << std::endl;
+        return 1;
+    }
 
-   // Parse the URL
-   ParsedUrl url( argv[ 1 ] );
+    // Parse the URL
+    ParsedUrl url(argv[1]);
 
-   // Get the host address.
+    // Get the host address.
+    struct addrinfo *address, hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    std::cout << url.Host << std::endl;
+    getaddrinfo(url.Host, "80", &hints, &address);
 
-   // Create a TCP/IP socket.
+    url.printURL();
 
-   // Connect the socket to the host address.
+    // Create a TCP/IP socket.
+    int socketFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-   // Send a GET message.
+    // Connect the socket to the host address.
+    printAddrInfo(*address);
+    int connectResult =
+        connect(socketFD, address->ai_addr, address->ai_addrlen);
+    std::cout << "working" << std::endl;
 
-   // Read from the socket until there's no more data, copying it to
-   // stdout.
+    // Send a GET message.
+    std::string req =
+        "GET / HTTP/1.1\r\nHost: www.nytimes.com\r\nUser-Agent: "
+        "LinuxGetUrl/2.0 ikansal@umich.edu (Linux)\r\nAccept: "
+        "*/*\r\nAccept-Encoding: identity\r\nConnection: close\r\n\r\n";
 
-   // Close the socket and free the address info structure.
+    send(socketFD, req.c_str(), req.length(), 0);
 
-   }
+    // Read from the socket until there's no more data, copying it to
+    // stdout.
+    char buffer[10240];
+    int bytes;
+    while ((bytes = recv(socketFD, buffer, sizeof(buffer), 0)) > 0) {
+        write(1, buffer, bytes);
+        printf(buffer);
+    }
+
+    // Close the socket and free the address info structure.
+    close(socketFD);
+    freeaddrinfo(address);
+}
