d4c82e688a3293d05462b3e6f24ff7c3189da84d
Jason Lin <linjay@umich.edu>
2025-03-14 17:29:21 -0400

    added funcs to save and load bf

--- BloomFilterStarterFiles/BloomFilter.h
+++ BloomFilterStarterFiles/BloomFilter.h
@@ -6,6 +6,7 @@
    #include <string.h>
    #include <cstring>
    #include <openssl/md5.h>
+   #include <unistd.h>
 
    class Bloomfilter
       {
@@ -19,6 +20,56 @@
             numHashFuncs = round((sizeInBits/num_objects) * log(2));
             }
 
+         Bloomfilter(int handle)
+         {
+
+            read(handle, reinterpret_cast<char*>(&sizeInBits), sizeof(sizeInBits));
+            read(handle, reinterpret_cast<char*>(&numHashFuncs), sizeof(numHashFuncs));
+            
+            size_t numBytes = (sizeInBits + 7) / 8;
+            std::vector<unsigned char> bytes(numBytes);
+            read(handle, reinterpret_cast<char*>(bytes.data()), numBytes);
+            int bitIndex = 0;
+            bloomFilter.reserve(sizeInBits);
+            for (size_t i = 0; i < numBytes; i++)
+            {
+               for (int bit = 0; bit < 8; bit++)
+               {
+                  if (i * 8 + bit >= sizeInBits)
+                     break;
+                  bool currentBit = (bytes[i] >> bit) & 1;
+                  bloomFilter[bitIndex++] = currentBit;
+               }
+            }
+         }
+         
+         void writeBFtoFile(int handle)
+         {
+            write(handle, reinterpret_cast<const char*>(&sizeInBits), sizeof(sizeInBits));
+            write(handle, reinterpret_cast<const char*>(&numHashFuncs), sizeof(numHashFuncs));
+            
+            // Pack bits into a byte then add to vector
+            std::vector<unsigned char> bytes;
+            int bitCount = 0;
+            unsigned char cur = 0;
+            for (bool bit : bloomFilter)
+            {
+               cur |= bit << bitCount;
+               bitCount ++;
+               if (bitCount == 8)
+               {
+                  bytes.push_back(cur);
+                  cur = 0;
+                  bitCount = 0;
+               }
+            }
+            // Add remaining bits.
+            if(bitCount > 0)
+            {
+               bytes.push_back(cur);
+            }
+            write(handle, reinterpret_cast<const char*>(bytes.data()), bytes.size());
+         }
          void insert( const std::string &s)
             {
             // Hash the string into two unique hashes.
