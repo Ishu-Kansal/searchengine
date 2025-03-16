9a3b9ba475f8b28c541705fc21f2b1ad0e397b30
Jason Lin <linjay@umich.edu>
2025-03-16 14:54:57 -0400

    Changed add_word in IndexChunk to work with deltas

--- src/inverted_index/Index.h
+++ src/inverted_index/Index.h
@@ -1,13 +1,63 @@
 #include<vector>
 
 #include "utils/unrolled_linked_list.h"
+#include "utils/cunique_ptr.h"
 #include "HashTable/HashTableStarterFiles/HashTable.h"
 
+using delta_t = unsigned char;
+
+size_t IndicatedLength(const delta_t del[]) {
+    uint16_t mask = 1 << 7;
+    size_t numSet = 0;
+    while (*del & mask) {
+        ++numSet;
+        mask >>= 1;
+    }
+    if (numSet < 1 || numSet > 6) return 1;
+    return numSet;
+    }
+size_t SizeOfDelta(int offset) {
+    return (offset < 0x80) + (offset < 0x800) + (offset < 0x10000) +
+        (offset < 0x200000) + (offset < 0x4000000);
+  }
+
+int delta_to_int(const delta_t del[]) {
+    static const delta_t block_mask = (1 << 6) - 1;
+    const size_t num_bytes = IndicatedLength(del);
+    if (num_bytes == 1) return del[0];
+    int cur = [&](const delta_t first) {
+      return first & ((1 << (7 - num_bytes)) - 1);
+    }(del[0]);
+    for (int i = 1; i < num_bytes; ++i) {
+      cur <<= 6;
+      cur |= (del[i] & block_mask);
+    }
+    return cur;
+  }
+
+delta_t *int_to_delta(int offset) {
+    static const delta_t block_mask = (1 << 6) - 1;
+    const size_t num_bytes = SizeOfDelta(offset);
+    switch (num_bytes) {
+      case 1:
+        return new delta_t(offset);
+      default:
+        delta_t *del = new delta_t[num_bytes];
+        for (int cur = num_bytes - 1; cur >= 0; --cur) {
+          del[cur] = num_bytes & block_mask;
+        }
+        del[0] |= ((1 << num_bytes) - 1) << (8 - num_bytes);
+        return del;
+    }
+  }
 struct Post
 {
-    uint32_t pos;
-    bool title;
-    bool bold;
+    cunique_ptr<delta_t[]> delta{};
+    bool title = false;
+    bool bold = false;
+    
+    Post() = default;
+    Post(cunique_ptr<delta_t[]> d, bool t, bool b) : delta(std::move(d)), title(t), bold(b) {}
 };
 
 class IndexChunk {
@@ -25,16 +75,14 @@ class IndexChunk {
             bool in_bold = false, 
             bool new_document = true)
         {
-            Post new_post;
-            new_post.pos = pos;
-            new_post.title = in_title;
-            new_post.bold = in_bold;
-            
-            inverted_word_index.add_word(word, new_post, new_document);
+            prev_pos = pos;
+
+            inverted_word_index.add_word(word, Post(int_to_delta(pos - prev_pos), in_title, in_bold), new_document);
         }
     private:
         std::vector<std::string> url_list;
         InvertedIndex inverted_word_index;
+        uint64_t prev_pos;
 
 };
 
