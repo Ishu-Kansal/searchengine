6afb74958aee09e5083befc8a733262e8187257c
Ishu-Kansal <ikansal22@gmail.com>
2025-04-22 01:14:33 -0400

    deleted files and redid constants

--- src/crawler/constants.h
+++ src/crawler/constants.h
@@ -4,11 +4,11 @@
 
 constexpr int MAX_EXPECTED_LINKS = 10'000'000;
 constexpr uint32_t MAX_PROCESSED = 100'000;
-constexpr uint64_t MAX_QUEUE_SIZE = 10'000;
-constexpr uint64_t MAX_VECTOR_SIZE = 100'000;
-constexpr uint64_t NUM_RANDOM = 15'000;
-constexpr uint64_t TOP_K_ELEMENTS = 20000;
-constexpr uint64_t DISPATCHER_SAVE_RATE = 25'000;
+constexpr uint64_t MAX_QUEUE_SIZE = 50'000;
+constexpr uint64_t MAX_VECTOR_SIZE = 1'000'000;
+constexpr uint64_t NUM_RANDOM = 25'000;
+constexpr uint64_t TOP_K_ELEMENTS = 50'000;
+constexpr uint64_t DISPATCHER_SAVE_RATE = 50'000;
 constexpr double MAX_FALSE_POSITIVE_RATE = 1e-2;
 
 using header_t = uint64_t;
