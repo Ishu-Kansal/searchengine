17d321f292b46055c1f229e4f9855a3f7384b923
Anton Petushkov <antonpet@umich.edu>
2025-04-23 15:20:27 -0400

    Synced constants.h

--- src/crawler/constants.h
+++ src/crawler/constants.h
@@ -3,7 +3,7 @@
 #include <cstdint>
 
 constexpr int MAX_EXPECTED_LINKS = 10'000'000;
-constexpr uint32_t MAX_PROCESSED = 10'000;
+constexpr uint32_t MAX_PROCESSED = 100'000;
 constexpr uint64_t MAX_QUEUE_SIZE = 10'000;
 constexpr uint64_t MAX_VECTOR_SIZE = 100'000;
 constexpr uint64_t NUM_RANDOM = 15'000;
@@ -11,7 +11,7 @@ constexpr uint64_t TOP_K_ELEMENTS = 7'500;
 constexpr uint64_t DISPATCHER_SAVE_RATE = 25'000;
 constexpr double MAX_FALSE_POSITIVE_RATE = 1e-2;
 
-constexpr uint64_t NUM_CHUNKS = 1;  // start small
+constexpr uint64_t NUM_CHUNKS = 10;  // start small
 
 using header_t = uint64_t;
 constexpr uint64_t GET_COMMAND = 1;
